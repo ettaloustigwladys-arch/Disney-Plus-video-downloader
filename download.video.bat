dca2791922c5c7d403e05ed43672cd036af91ebb
ettaloustigwladys-arch <ettaloustigwladys@gmail.com>
2026-05-20 15:19:31 +0200

    Update download.video.bat

--- download.video.bat
+++ download.video.bat
@@ -1,3 +1,3 @@
 @echo off
-disneyplus.py --url https://www.disneyplus.com/es-es/series/loki/6pARMvILBGzF -q 720 -s 1 -e 1 --alang eng --slang eng --flang eng
-pause
+disneyplus.py --url https://www.disneyplus.com/fr-fr/browse/entity-38f451b9-22df-4784-b580-4e4db4b92dba -q 720 -s 1 -e 3 --alang eng --slang eng --flang eng
+play
